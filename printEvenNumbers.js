d491fc0a9397cfb7d57a416de046af80407d4ac4
raviranjan mishra <raviranjanmishra767@gmail.com>
2025-10-30 12:09:36 +0530

    1 to nth

---
+++ printEvenNumbers.js
@@ -0,0 +1,10 @@
+function Chechevenodd(num)
+{
+    if(num%2 == 0)
+    {
+        console.log("this is even Eumber ",num)
+    }else{
+        console.log("this is  a Odd number ",num)
+    }
+}
+Chechevenodd(2)
